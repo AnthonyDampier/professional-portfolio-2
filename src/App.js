5210500b97c81634a694433b50f0af137a91b039
AnthonyDampier <Dampieranthony2@gmail.com>
2024-02-27 13:39:49 -0600

    Add routing and pages to App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+
+// Pages
+import Home from './Pages/Home/Home';
+import Projects from './Pages/Projects/Projects';
+import ProfessionalResources from './Pages/ProfessionalResources/ProfessionalResources';
+import Network from './Pages/Network/Network';
+import AboutMe from './Pages/AboutMe/AboutMe';  
+import ContactMe from './Pages/ContactMe/ContactMe';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        {/* <DraggableBox userInfo={userInfo} userFinancialInfo={userFinancialInfo} userMortgageInfo={userMortgageInfo} userVehicleInfo={userVehicleInfo}/> */}
+        {/* <NavBar /> */}
+          <Routes>
+            {/* Home */}
+            <Route path="/" exact 
+              element={
+                <Home />
+              }>
+            </Route>
+            {/* Projects */}
+            <Route path="/Projects" exact 
+              element={
+                <Projects />
+              }>
+            </Route>
+            {/* Professional Resources */}
+            <Route path="/ProfessionalResources" exact 
+              element={
+                <ProfessionalResources />
+              }>
+            </Route>
+            {/* Network */}
+            <Route path="/Network" exact 
+              element={
+                <Network />
+              }>
+            </Route>
+            {/* About */}
+            <Route path="/About" exact 
+              element={
+                <AboutMe />
+              }>
+            </Route>
+            {/* ContactMe */}
+            <Route path="/Contact" exact 
+              element={
+                <ContactMe />
+              }>
+            </Route>
+          </Routes>
+        {/* <Footer /> */}
+      </div>
+    </Router>
   );
 }
 

3cc45df4c7e684d328dbf4c954f7499e28707146
AnthonyDampier <Dampieranthony2@gmail.com>
2024-02-28 12:02:40 -0600

    Update Hero styles and images (Info, Hero Image, & Iphone)

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,19 +4,31 @@ import Iphone from "../Iphone/Iphone";
 
 const Hero = ({linkedIn, GitHubProfile}) => {
     return (
-        <div>
-            <div className="hero">
+        <div className="hero">
+            <div className="hero-info">
+                <h3 className="salutations">WELCOME! My name is</h3>
                 <h1 className="name">Anthony Dampier</h1>
-                <h2 className="title">Developer / Project Manager</h2>
-                <p>Talk about me</p>
-                <button className="about-me">Learn More</button>
-                <div>   
-                    <a href={linkedIn} target="_blank" rel="noopener noreferrer"><img src="https://img.icons8.com/ios/50/000000/linkedin.png" alt="LinkedIn"/></a>
-                    <a href={GitHubProfile} target="_blank" rel="noopener noreferrer"><img src="https://img.icons8.com/ios/50/000000/github.png" alt="GitHub"/></a>
+                <h2 className="title">I'm a Developer and Project Manager</h2>
+                <p>with multi-dimensional skill-set to the forefront of Salesforce consulting and development. With a strong background in Salesforce Software Development, React JS Application Development, Solution Management, and DevOps, I have delivered innovative and effective solutions for various clients at Digital Mass, a leading Salesforce consulting firm. Some of my notable achievements include:</p>
+                <p>Developing and deploying a custom Salesforce app for a large education client, using Lightning Web Components, Tailwind CSS, and HTML5, resulting in improved user experience and functionality.</p>
+                <p>Implementing a React JS and Next JS web application for clients, using RESTful APIs, Firebase, and Cloud Functions.</p>
+                <p>Managing the end-to-end project lifecycle for multiple Salesforce projects, from scoping and planning to testing and deployment, using agile methodologies, tools, and best practices, ensuring quality, efficiency, and customer satisfaction.</p>
+                <button className="about-me">Check out my resume! CLICK HERE!!</button>
+                <div className="hero-links">   
+                    <a href={GitHubProfile} class="social-icon" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+                        <i class="fab fa-linkedin-in"></i>
+                    </a>
+                    <a href={linkedIn} class="social-icon" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
+                        <i class="fab fa-github"></i>
+                    </a>
                 </div>
             </div>
-            <img src="https://via.placeholder.com/300" alt="Hero"/>
-            <Iphone />
+            <div className="hero-image">
+                <img src="../Assets/ProfilePhoto.jpeg" alt="Hero"/>
+            </div>
+            <div className="hero-phone">
+                <Iphone />
+            </div>
         </div>
     );
 }

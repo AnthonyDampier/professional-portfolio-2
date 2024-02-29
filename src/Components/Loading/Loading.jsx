f6d5753f4b098ece6e456c0d0c457ea88103edb1
AnthonyDampier <Dampieranthony2@gmail.com>
2024-02-29 10:54:21 -0600

    Add loading animation styles and element to Loading component

--- src/Components/Loading/Loading.jsx
+++ src/Components/Loading/Loading.jsx
@@ -4,6 +4,7 @@ import './Loading.css';
 const Loading = () => {
     return (
         <div className="loading">
+            {/* https://codepen.io/rajveer01_pen/pen/qBvWWRe */}
             <div class="box"></div>
         </div>
     );

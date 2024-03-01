38ecf746c14bb88707aa8cfd5cb8230e3e7fef82
AnthonyDampier <Dampieranthony2@gmail.com>
2024-03-01 09:42:36 -0600

    Refactor ProjectPanel component

--- src/Components/ProjectPanel/ProjectPanel.jsx
+++ src/Components/ProjectPanel/ProjectPanel.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Microlink from '@microlink/react';
 import GitActivity from "../../Components/GitActivity/GitActivity";
 import Loading from "../../Components/Loading/Loading";
@@ -11,59 +11,47 @@ const ProjectPanel = () => {
     const [error, setError] = useState(null);
     const [repoSet, setRepoSet] = useState([]);
 
-    // Set the loading to true after 2 seconds
-    setTimeout(() => {
-        if (loading){
-            setLoading(false);
-        }
-    }, 2000); // 2000 milliseconds = 2 seconds
-
-
-    // const [githubToken, setGithubToken] = useState(env.REACT_APP_GITHUB_TOKEN);
-    let githubToken = process.env.REACT_APP_GITHUB_TOKEN;
-     // Load environment variables from .env file
-    const fetchEventsContributions = async () => { 
-        setLoading(true);
-        try {
-            setRepoSet(await getUserRepositories(username, process.env.REACT_APP_GITHUB_TOKEN));
-        } catch (error) {
-            setError(error.message);
-        } finally {
-
-        }
-    }
-
     useEffect(() => {
+        const fetchEventsContributions = async () => {
+            setLoading(true);
+            try {
+                setRepoSet(await getUserRepositories(username, process.env.REACT_APP_GITHUB_TOKEN));
+            } catch (error) {
+                setError(error.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+
         fetchEventsContributions();
+    }, [username]);
 
-    }, [username, githubToken]);
-
-    if (loading) return <div><Loading/></div>;
+    if (loading) return <Loading />;
     if (error) return <div>Error: {error}</div>;
-    
-    return (
 
+    return (
         <div className='project-panel'>
             <div className='feature-section'>
-                <GitActivity username="AnthonyDampier"/>
+                <GitActivity username={username} />
                 <div className='grid-container'>
-                {repoSet.length !== 0 && !loading ? 
-                            repoSet.map((repoName, index) => (
-                                <div key={index} className="grid-item">
-                                    <Microlink url={`https://github.com/${username}/${repoName}/`}
-                                        lazy={{ threshold: 0.01 }}
-                                        media='auto'
-                                        size='medium'
-                                    />
-                                </div>
-                            )) 
-                            : ( loading ? <div className='loading'>`<Loading/>`</div> : (<div>`Error: ${error}`</div>))
-                        }           
-                        </div>
-                    </div>
+                    {repoSet.length !== 0 ? (
+                        repoSet.map((repoName, index) => (
+                            <div key={index} className="grid-item">
+                                <Microlink
+                                    url={`https://github.com/${username}/${repoName}/`}
+                                    lazy={{ threshold: 0.01 }}
+                                    media='auto'
+                                    size='medium'
+                                />
+                            </div>
+                        ))
+                    ) : (
+                        <div>No repositories found.</div>
+                    )}
+                </div>
+            </div>
         </div>
-
     );
-}
+};
 
 export default ProjectPanel;

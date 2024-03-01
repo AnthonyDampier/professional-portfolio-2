1067a9e733b2d8a7404b3d91e92f3da32283071e
AnthonyDampier <Dampieranthony2@gmail.com>
2024-03-01 10:06:41 -0600

    Refactor getUserContributions function to use ES6 import syntax and improve date formatting

--- src/Utils/GetUserGitHubContributions/GetUserGitContributions.jsx
+++ src/Utils/GetUserGitHubContributions/GetUserGitContributions.jsx
@@ -1,27 +1,31 @@
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
 
-const getUserContributions = async (username, token) =>{
+const getUserContributions = async (username, token) => {
   const headers = {
     Authorization: `token ${token}`,
   };
 
   const eventsResponse = await fetch(`https://api.github.com/users/${username}/events`, { headers });
   const events = await eventsResponse.json();
-  const repoSet = events.map(event => event.repo.name);
+  const repoSet = new Set(events.map(event => event.repo.name));
 
-  let totalCommits = 0;
-
-  let index = 0;
   let dateCommitMap = {};
+
   for (const repo of repoSet) {
     const [owner, repoName] = repo.split('/');
     const commitsResponse = await fetch(`https://api.github.com/repos/${owner}/${repoName}/commits`, { headers });
     const commits = await commitsResponse.json();
-    if (commits[0] !== undefined){
-        const dt = new Date(commits[0].commit.author.date);
-        const month = (dt.getMonth() + 1).toString().length === 1 ? "0" + (dt.getMonth() + 1) : (dt.getMonth() + 1);
-        const commitDT = dt.getFullYear() + "-" + (month) + "-" + dt.getDate();
-        dateCommitMap[commitDT] = commits.length + dateCommitMap[commitDT] || commits.length;
+
+    for (const commit of commits) {
+      const dt = new Date(commit.commit.author.date);
+      const month = (dt.getMonth() + 1).toString().padStart(2, '0');
+      const commitDT = `${dt.getFullYear()}-${month}-${dt.getDate()}`;
+
+      if (dateCommitMap[commitDT] === undefined) {
+        dateCommitMap[commitDT] = 1;
+      } else {
+        dateCommitMap[commitDT]++;
+      }
     }
   }
 

3e3e2243a57381bf3bf6d689beef767cfa6c80db
AnthonyDampier <Dampieranthony2@gmail.com>
2024-03-01 09:43:40 -0600

    Refactor GitActivity component

--- src/Components/GitActivity/GitActivity.jsx
+++ src/Components/GitActivity/GitActivity.jsx
@@ -1,70 +1,62 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Calendar from 'react-github-contribution-calendar';
 import Loading from "../Loading/Loading";
 import getUserContributions from '../../Utils/GetUserGitHubContributions/GetUserGitContributions.jsx';
 import './GitActivity.css';
 import DogWalking from "../DogWalking/DogWalking.jsx";
 
+const GitActivity = ({ username }) => {
+  const [events, setEvents] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [dateCommitMap, setDateCommitMap] = useState({});
 
-const GitActivity = ({username}) => {const [events, setEvents] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    const [dateCommitMap, setDateCommitMap] = useState({});
-    // const [githubToken, setGithubToken] = useState(env.REACT_APP_GITHUB_TOKEN);
-    let githubToken = process.env.REACT_APP_GITHUB_TOKEN;
-     // Load environment variables from .env file
-    const fetchEventsContributions = async () => { 
-      setLoading(true);
-      try {
-        setDateCommitMap(await getUserContributions(username, process.env.REACT_APP_GITHUB_TOKEN));
-      } catch (error) {
-        setError(error.message);
-      } finally {
- 
-      }
+  const fetchEventsContributions = async () => {
+    setLoading(true);
+    try {
+      setDateCommitMap(await getUserContributions(username, process.env.REACT_APP_GITHUB_TOKEN));
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
     }
+  }
 
-    setTimeout(() => {
-        if (loading){
-            setLoading(false);
-        }
-    }, 2000); // 2000 milliseconds = 2 seconds
+  useEffect(() => {
+    if (process.env.REACT_APP_GITHUB_TOKEN && loading) {
+      fetchEventsContributions();
+    }
+  }, [username, loading]);
 
-    useEffect(() => {
-      if (githubToken && loading){
-        fetchEventsContributions();
-      }
-    }, [githubToken, username, loading]);
-  
-    if (loading) return <div><Loading/></div>;
-    if (error) return <div>Error: {error}</div>;
-  
-    return (
-      <div className="github-activity">
-        <h1>GitHub Activity</h1>
-        <p>* Accounts for only public repositories</p>
-        {/* Reference: https://haripo.github.io/react-github-contribution-calendar/ */}
-        <Calendar 
-          style={{ width: '400px', height: 'auto' }}
-          values={dateCommitMap} 
-          until={Date.Today}
-          panelColors={['#fff', '#c6e48b', '#7bc96f', '#239a3b', '#196127']}
-          panelAttributes={{ 'rx': 0, 'ry': 0 }}
-          weekLabelAttributes={{'rotate': 0}}
-          monthLabelAttributes={{
-            style: {
-              'text-decoration': 'none',
-              'font-size': '8px',
-              'alignmentBaseline': 'central',
-              'fill': '#fff'
-            }
-            }}
-          />
-          <div className="dogComponent">
-           <DogWalking/>
-          </div>
+  if (loading) return <Loading />;
+  if (error) return <div>Error: {error}</div>;
+
+  return (
+    <div className="github-activity">
+      <h1>GitHub Activity</h1>
+      <p>* Accounts for only public repositories</p>
+      {/* Reference: https://haripo.github.io/react-github-contribution-calendar/ */}
+      <Calendar
+        style={{ width: '400px', height: 'auto' }}
+        values={dateCommitMap}
+        until={new Date()}
+        panelColors={['#fff', '#c6e48b', '#7bc96f', '#239a3b', '#196127']}
+        panelAttributes={{ 'rx': 0, 'ry': 0 }}
+        weekLabelAttributes={{ 'rotate': 0 }}
+        monthLabelAttributes={{
+          style: {
+            'text-decoration': 'none',
+            'font-size': '8px',
+            'alignmentBaseline': 'central',
+            'fill': '#fff'
+          }
+        }}
+      />
+      <div className="dogComponent">
+        <DogWalking />
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default GitActivity;

132b1b6e8d95655cc054a554f0ac2d7e9dceffd8
AnthonyDampier <Dampieranthony2@gmail.com>
2024-03-01 09:49:32 -0600

    Refactor getUserContributions function to use ES6 import syntax and simplify repoSet mapping

--- src/Utils/GetUserRepositories/GetUserRepositories.jsx
+++ src/Utils/GetUserRepositories/GetUserRepositories.jsx
@@ -1,17 +1,15 @@
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
 
-const getUserContributions = async (username, token) =>{
+const getUserContributions = async (username, token) => {
   const headers = {
     Authorization: `token ${token}`,
   };
 
   const eventsResponse = await fetch(`https://api.github.com/users/${username}/events`, { headers });
   const events = await eventsResponse.json();
-  const repoSet = Array.from(new Set(events.map(event => event.repo.name)));
-  for (let i = 0; i < repoSet.length; i++){
-    const [owner, repoName] = repoSet[i].split('/');
-    repoSet[i] = repoName;
-  }
+  const repoSet = Array.from(new Set(events.map(event => event.repo.name)))
+    .map(repo => repo.split('/')[1]);
+  
   return repoSet;
 }
 

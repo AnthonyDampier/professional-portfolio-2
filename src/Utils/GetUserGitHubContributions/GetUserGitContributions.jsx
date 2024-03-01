0132472cf30845fdd59981b9cbe1f8f5913cb00b
AnthonyDampier <Dampieranthony2@gmail.com>
2024-03-01 11:58:38 -0600

    Refactor getUserContributions function to improve performance

--- src/Utils/GetUserGitHubContributions/GetUserGitContributions.jsx
+++ src/Utils/GetUserGitHubContributions/GetUserGitContributions.jsx
@@ -9,19 +9,19 @@ const getUserContributions = async (username, token) =>{
   const events = await eventsResponse.json();
   const repoSet = events.map(event => event.repo.name);
 
-  let totalCommits = 0;
-
-  let index = 0;
   let dateCommitMap = {};
   for (const repo of repoSet) {
     const [owner, repoName] = repo.split('/');
     const commitsResponse = await fetch(`https://api.github.com/repos/${owner}/${repoName}/commits`, { headers });
     const commits = await commitsResponse.json();
-    if (commits[0] !== undefined){
-        const dt = new Date(commits[0].commit.author.date);
-        const month = (dt.getMonth() + 1).toString().length === 1 ? "0" + (dt.getMonth() + 1) : (dt.getMonth() + 1);
-        const commitDT = dt.getFullYear() + "-" + (month) + "-" + dt.getDate();
-        dateCommitMap[commitDT] = commits.length + dateCommitMap[commitDT] || commits.length;
+
+    for (let i = 0; i < commits.length; i++){
+      const dt = new Date(commits[i].commit.author.date);
+      const month = (dt.getMonth() + 1).toString().length === 1 ? "0" + (dt.getMonth() + 1) : (dt.getMonth() + 1);
+      const commitDT = dt.getFullYear() + "-" + (month) + "-" + dt.getDate();
+      console.log(commitDT);
+      console.log(repoName);
+      dateCommitMap[commitDT] = commits.length + dateCommitMap[commitDT] || commits.length;
     }
   }
 
